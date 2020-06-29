0d3b0b20b26f80211508e4f2f57c1aba0aa81fd7
goodyp-benjyyang <goodyp12323@qq.com>
2020-06-29 23:23:39 +0800

    first push

---
+++ src/test/java/top/goodyp/springbootdemo2/Springbootdemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.goodyp.springbootdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootdemo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
